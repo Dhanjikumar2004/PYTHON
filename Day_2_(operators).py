02fbbf37b46ff94787cd083f9e22787444d9b277
Dhanjikumar2004 <dhanjikumar2004@gmail.com>
2025-01-08 20:39:09 +0530

    Day_2_(operators)

---
+++ Day_2_(operators).py
@@ -0,0 +1,21 @@
+# Calculate the area of a circle.
+radius = float(input("Enter the radius of the circle: "))
+area = 3.14 * radius * radius
+print(f"The area of the circle with radius {radius} is: {area:.2f}")
+
+# Calculate the area of a triangle.
+b = float(input("enter the value of base "))
+h = float(input("enter the value of height "))
+area = 0.5 * b *h
+print("area of triangle = ",area)
+
+# Calculate the area of a rectangle.
+length = int(input("entr the length value : "))
+wedth = int(input("entr the wedth value : "))
+area = length * wedth
+print("area of rectangle = ",area)
+
+# Calculate the area of a square.
+side = int(input("enter the value : "))
+area = side * side
+print("are of square = ",area)
